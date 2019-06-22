fdf9fb0b86c11fe5efce11351c19007d29715ea7
Alex Plescan <alexpls@fastmail.com>
2019-06-22 13:34:12 +1000

    Auto launching wills updates url hash

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { WillLauncher } from './WillLauncher'
+const AUTO_LAUNCH_HASH = '#auto'
 
 const launcher = new WillLauncher()
 launcher.listenForDomEventsAndLaunchWills()
@@ -6,12 +7,22 @@ launcher.animate()
 
 const autoLaunchElem = document.getElementById('auto-launch')
 
-autoLaunchElem.addEventListener('mousedown', e => {
+if (document.location.hash === AUTO_LAUNCH_HASH) {
+  toggleAutoLaunch()
+}
+
+autoLaunchElem.addEventListener('click', e => {
   e.preventDefault()
+  toggleAutoLaunch()
+})
+
+function toggleAutoLaunch () {
   launcher.toggleAutoLaunch()
   if (launcher.isAutoLaunching) {
     autoLaunchElem.innerText = 'stop automatically launching wills'
+    document.location.hash = AUTO_LAUNCH_HASH
   } else {
     autoLaunchElem.innerText = 'start automatically launching wills'
+    document.location.hash = ''
   }
-})
+}

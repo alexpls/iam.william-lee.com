a672cdf092918ca0f5d0415100b401703a913f51
Ben <bthompson4163@gmail.com>
2019-06-23 16:04:45 +1000

    Shake doesn't work on HTTP

--- src/WillLauncher.ts
+++ src/WillLauncher.ts
@@ -1,11 +1,10 @@
-import { Konami } from './Konami'
-import { ShakenBake } from './ShakenBake'
-import { WillHeadImage } from './WillHeadImage'
-import { ImageParticle } from './ImageParticle'
-import { Point } from './Point'
 import { CanvasCreator } from './CanvasCreator'
-import { Size } from './Size'
+import { ImageParticle } from './ImageParticle'
+import { Konami } from './Konami'
+import { Point } from './Point'
 import { randomNumberBetween } from './randomNumberBetween';
+import { Size } from './Size'
+import { WillHeadImage } from './WillHeadImage'
 
 export class WillLauncher {
   private image = new WillHeadImage()
@@ -68,7 +67,6 @@ export class WillLauncher {
     }, false)
 
     this.konami.listen()
-    new ShakenBake(this.image)
   }
 
   animate (): void {
